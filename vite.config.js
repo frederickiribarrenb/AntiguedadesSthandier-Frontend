e7e0d015a672bd4b7ef9a83ffc1857fabfc82343
Frederick Iribarren <frederickiribarren@gmail.com>
2026-07-09 18:08:49 -0400

    se crea workflows para hacer deploy cada vez que se modifique main

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/antiguedades-sthandier/', // Cambia 'proyecto' por el nombre de tu repo si es diferente
+  base: 'https://frederickiribarrenb.github.io/AntiguedadesSthandier-Frontend', // Cambia 'proyecto' por el nombre de tu repo si es diferente
 })
